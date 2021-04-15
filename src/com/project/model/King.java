1579c83428fa711e9e24be5dac7f581a3db0dfa6
Quentin-dot-prog <75437365+Quentin-dot-prog@users.noreply.github.com>
2021-04-15 17:19:50 +0200

    Create King.java

---
+++ src/com/project/model/King.java
@@ -0,0 +1,13 @@
+package com.project.model;
+
+public class King {
+    String kingName;
+
+    public void setKingName(String kingName) {
+        this.kingName = kingName;
+    }
+
+    public String getKingName() {
+        return this.kingName;
+    }
+}
